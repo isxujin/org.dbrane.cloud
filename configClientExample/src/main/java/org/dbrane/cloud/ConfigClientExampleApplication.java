2ad402c97e551cc5ed00890270e7043f3aa5e6f9
xujin <xujin@xujinwin.hs.handsome.com.cn>
2017-02-07 14:18:53 +0800

    update projects

--- configClientExample/src/main/java/org/dbrane/cloud/ConfigClientExampleApplication.java
+++ configClientExample/src/main/java/org/dbrane/cloud/ConfigClientExampleApplication.java
@@ -1,23 +1,11 @@
 package org.dbrane.cloud;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ConfigClientExampleApplication {
 
-	@Value("${name:World!}")
-    String bar;
-
-    @RequestMapping("/")
-    String hello() {
-        return "Hello " + bar + "!";
-    }
-    
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientExampleApplication.class, args);
 	}
