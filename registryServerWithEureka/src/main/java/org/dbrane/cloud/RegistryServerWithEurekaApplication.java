f542eac9d54a5cd984ee2fdec7ea28745f8cc598
xujin <xujin@xujinwin.hs.handsome.com.cn>
2017-02-13 09:16:56 +0800

    update project

---
+++ registryServerWithEureka/src/main/java/org/dbrane/cloud/RegistryServerWithEurekaApplication.java
@@ -0,0 +1,12 @@
+package org.dbrane.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServerWithEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServerWithEurekaApplication.class, args);
+	}
+}
