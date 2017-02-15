9118a5926aba8dfc8c2cceed21cc15f9e134ee9d
xujin <xujin@xujinwin.hs.handsome.com.cn>
2017-02-15 14:40:47 +0800

    Update project

--- configClientExample/src/main/java/org/dbrane/cloud/ConfigClientExampleApplication.java
+++ configClientExample/src/main/java/org/dbrane/cloud/ConfigClientExampleApplication.java
@@ -1,9 +1,11 @@
 package org.dbrane.cloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ConfigClientExampleApplication {
 
 	public static void main(String[] args) {
