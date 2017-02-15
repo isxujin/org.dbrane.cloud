2c917145bdc861cec572a263e83d37065b8c741a
xujin <xujin@xujinwin.hs.handsome.com.cn>
2017-02-15 15:22:40 +0800

    create project

--- restClientWithFeign/src/main/java/org/dbrane/cloud/RestClientWithFeignApplication.java
+++ restClientWithFeign/src/main/java/org/dbrane/cloud/RestClientWithFeignApplication.java
@@ -1,9 +1,13 @@
 package org.dbrane.cloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@RestController
 public class RestClientWithFeignApplication {
 
 	public static void main(String[] args) {
